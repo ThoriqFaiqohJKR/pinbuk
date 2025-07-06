2dd4ef523ad464649b1760163b52c2d6abc258cd
Thoriq Faiqoh <thoriqgarena@gmail.com>
2025-07-07 04:55:25 +0700

    revisi footer 2

--- resources/views/layouts/testing.blade.php
+++ resources/views/layouts/testing.blade.php
@@ -188,7 +188,7 @@
 
   <!--footer-->
   <footer class="border-t border-red-600 mt-auto bg-white poppins-regular">
-    <div class="flex sm:flex-row items-start sm:items-center px-4 sm:px-8 md:px-12 lg:px-20 xl:px-44 py-4 border-b-2">
+    <div class="flex sm:flex-row items-start  px-4 sm:px-8 md:px-12 lg:px-20 xl:px-44 py-4 border-b-2">
         <div class="w-full flex justify-center sm:justify-start">
         <img
           src="{{asset('foto/Logo Auriga.png') }}"
